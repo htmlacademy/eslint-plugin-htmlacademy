dededebed2bbb82a5102ab75a1d85f8a8b67e52e
Sergey Kluchkovsky <kaineer@gmail.com>
2021-03-05 12:26:49 +0500

    Добавляет правило для
    https://app.clickup.com/t/db4k6p

---
+++ tests/lib/rules/no-boolean-return.js
@@ -0,0 +1,34 @@
+/**
+ * @fileoverview Should not return true/false from if/else
+ * @author Sergey Kluchkowsky
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-boolean-return");
+const {RuleTester} = require("eslint");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("no-boolean-return", rule, {
+
+  valid: [
+    "var fn = function (value) { return (value === 5); }"
+  ],
+
+  invalid: [
+    {
+      code: "var fn = function (value) { if (value === 5) { return true; } else { return false; } }",
+      errors: [{
+        message: "If statement may be replaced with return statement",
+        type: "IfStatement"
+      }]
+    }
+  ]
+});
