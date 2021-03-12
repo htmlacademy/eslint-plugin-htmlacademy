1f54b776c87fa57b4435f93a97f4dc5fd989a5b7
Sergey Kluchkovsky <kaineer@gmail.com>
2021-03-12 15:55:35 +0500

    Добавляет тест для переменных
    инициализированных функциями

--- tests/lib/rules/value-single-noun.js
+++ tests/lib/rules/value-single-noun.js
@@ -34,6 +34,10 @@ ruleTester.run("value-single-noun", rule, {
     "function check (cb) {}",
     "try {} catch (err) {}",
     "for (let i = 0; i < 10; i++) {}",
+
+    // Do not report error for functions
+    "const getValue = () => null",
+    "const getAge = function () {}",
   ],
 
   invalid: [
