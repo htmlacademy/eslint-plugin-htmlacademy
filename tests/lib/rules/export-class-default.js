ba764725d6e4ba293639211f8bf9b28047b6f891
Sergey Kluchkovsky <kaineer@gmail.com>
2021-03-30 13:11:46 +0500

    Доделывает правило

---
+++ tests/lib/rules/export-class-default.js
@@ -0,0 +1,41 @@
+/**
+ * @fileoverview Class should be exported only as default
+ * @author Sergey Kluchkovsky
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/export-class-default");
+const {RuleTester} = require("eslint");
+
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: "module"
+  }
+});
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("export-class-default", rule, {
+  valid: [
+    "class Foo {}; export default Foo",
+    "export default class Foo {}"
+  ],
+
+  invalid: [
+    {
+      code: "export class Foo {}",
+      errors: [{
+        message: "Class «Foo» should be exported as default",
+        type: "ExportNamedDeclaration"
+      }]
+    }
+  ]
+});
