d188f208fc015b6d03355433ad927baafd85de87
Sergey Kluchkovsky <kaineer@gmail.com>
2023-04-19 13:09:02 +0500

    Добавляет тест для имени функции unblock

--- tests/lib/rules/function-starts-verb.js
+++ tests/lib/rules/function-starts-verb.js
@@ -19,7 +19,8 @@ ruleTester.run("function-starts-verb", rule, {
   valid: [
     "const getName = () => name;",
     "class Human { constructor() {} }",
-    "const initializeBounds = () => 0;"
+    "const initializeBounds = () => 0;",
+    "const unblock = () => 0;"
   ],
 
   invalid: [
